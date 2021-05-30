78b8f3a5b7d8a2efe4af27edb11a9c5fe52aea1b
ECheng742 <56359858+ECheng742@users.noreply.github.com>
2021-05-29 20:28:10 -0700

    Add Tone SM

--- source/main.c
+++ source/main.c
@@ -59,14 +59,109 @@ int Demo_Tick(int state) {
 	return state;
 }
 
+void set_PWM(double frequency) {
+    static double current_frequency;
+
+    if (frequency != current_frequency) {
+        if (!frequency) { TCCR3B &= 0x08; }
+        else { TCCR3B |= 0x03; }
+
+        if (frequency < 0.954) { OCR3A = 0xFFFF; }
+        else if (frequency > 31250) { OCR3A = 0x0000; }
+        else { OCR3A = (short) (8000000 / (128 * frequency)) - 1; }
+
+        TCNT3 = 0;
+        current_frequency = frequency;
+    } 
+}
+
+void PWM_on() {
+    TCCR3A = (1 << COM3A0);
+    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+    set_PWM(0);
+}
+
+void PWM_off() {
+    TCCR3A = 0x00;
+    TCCR3B = 0x00;
+}
+
+// TONE SM: When a button is pressed, a corresponding note is played
+// If multiple buttons pressed, nothing played
+enum TONE_States { TONE_SMStart, TONE_wait, TONE_note } TONE_State;
+
+double noteFrequency = 0x00;
+
+int ToneSMTick(int state) {
+    unsigned char button = (~PINB >> 7) & 0x1F;
+
+    switch(state) {
+        case TONE_SMStart:
+            state = TONE_wait;
+            break;
+
+        case TONE_wait:
+            if (!button) {
+                state = TONE_wait;
+            }
+            else { // button
+                state = TONE_note;
+            }
+            break;
+
+        case TONE_note:
+            if (!button) {
+                state = TONE_wait;
+            }
+            else { // button
+                state = TONE_note;
+            }
+            break;
+
+        default:
+            state = TONE_SMStart;
+            break;
+    } 
+
+    switch(state) {
+        case TONE_note:
+            if (button == 0x10) { // Note C - 261.63
+                noteFrequency = 261.63;
+            }
+            else if (button == 0x08) { // Note D - 293.66
+                noteFrequency = 293.66;
+            }
+            else if (button == 0x04) { // Note D - 329.63
+                noteFrequency = 329.63;
+            }
+            else if (button == 0x02) { // Note D - 349.23
+                noteFrequency = 349.23;
+            }
+            else if (button == 0x01) { // Note D - 392.00
+                noteFrequency = 392.00;
+            }
+            else { 
+                noteFrequency = 0x00;
+            }
+            break;
+
+        default:
+            break;
+            
+        set_PWM(noteFrequency);
+    }
+    return state;
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRB = 0x00; PORTB = 0xFF;
     DDRC = 0xFF; PORTC = 0x00;
     DDRD = 0xFF; PORTD = 0x00;
     /* Insert your solution below */
 
-    static task task1;
-    task *tasks[] = { &task1 };
+    static task task1, task2;
+    task *tasks[] = { &task1, &task2 };
     const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
     const char start = -1;
@@ -76,12 +171,18 @@ int main(void) {
     task1.elapsedTime = task1.period;
     task1.TickFct = &Demo_Tick;
 
+    task2.state = start;
+    task2.period = 200;
+    task2.elapsedTime = task2.period;
+    task2.TickFct = &ToneSMTick;
+
     unsigned short i;
     unsigned long GCD = tasks[0]->period;
     for ( i = 1; i < numTasks; i++ ) {
         GCD = findGCD(GCD,tasks[i]->period);
     }
 
+    PWM_on();
     TimerSet(GCD);
     TimerOn();
 
