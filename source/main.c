3f5617d4f7b257c7bc050092a1c44fd0ea272776
ECheng742 <56359858+ECheng742@users.noreply.github.com>
2021-05-30 19:47:28 -0700

    Add LED test

--- source/main.c
+++ source/main.c
@@ -268,7 +268,7 @@ int main(void) {
     task1.TickFct = &Demo_Tick;
 
     task2.state = start;
-    task2.period = 150;
+    task2.period = 200;
     task2.elapsedTime = task2.period;
     task2.TickFct = &ToneSMTick;
 
