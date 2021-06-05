9a2ddf272a89dbb840c39743925e54af536c7b12
ECheng742 <56359858+ECheng742@users.noreply.github.com>
2021-06-05 13:21:32 -0700

    Debug PENALTY SM

--- source/main.c
+++ source/main.c
@@ -303,7 +303,7 @@ int PenaltySMTick(int state) {
             state = PENALTY_SMStart;
             break;
     }
-    PORTA = checkFlag; // fix
+    PORTA = deductionsFlag; // fix
     return state;
 }
 
