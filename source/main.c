e82de5ff160832b913a1ddef57bef25421e7f36c
ECheng742 <56359858+ECheng742@users.noreply.github.com>
2021-06-05 19:16:53 -0700

    Debug LEVEL SM

--- source/main.c
+++ source/main.c
@@ -30,7 +30,7 @@ unsigned char levelFlag = 0x80;
 enum DISPLAY_States { DISPLAY_SMStart, DISPLAY_shift, DISPLAY_lost };
 int DisplaySMTick(int state) {
 	// Local Variables
-	static unsigned char pattern = 0x80;	// LED pattern - 0: LED off; 1: LED on
+	static unsigned char pattern = levelFlag;	// LED pattern - 0: LED off; 1: LED on
 	static unsigned char row = 0xFE;  	// (s) displaying pattern. 
 							            // 0: display pattern on row
 							            // 1: do NOT display pattern on row
@@ -69,7 +69,7 @@ int DisplaySMTick(int state) {
 	switch (state) {
         case DISPLAY_shift:	
             if (pattern == 0x01) { 
-				pattern = 0x80;
+				pattern = levelFlag;
                 rowFlag = 0x00;		   
                 numRow = (int) (rand() % 5 + 1);
                 if (numRow == 1) {
