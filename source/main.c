bdf90c5c98ab22bedd5db8a71cb97fc618d76781
ECheng742 <56359858+ECheng742@users.noreply.github.com>
2021-06-05 18:13:33 -0700

    Debug LEVEL SM

--- source/main.c
+++ source/main.c
@@ -91,8 +91,7 @@ int DisplaySMTick(int state) {
 			}
 			break;
         case DISPLAY_lost:
-            pattern = 0xFF;
-            row = (row << 1) | 0x01;
+            pattern = 0x00;
             break;
 		default:
 	        break;
